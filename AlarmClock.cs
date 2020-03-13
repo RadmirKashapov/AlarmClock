5a33b2a89a7fa1ea549df297a6339ebe091660bc
RadmirKashapov <radmir.kashapov.150700@gmail.com>
2020-03-13 14:19:35 +0300

    Added new timer function with System.Timers

--- AlarmClock.cs
+++ AlarmClock.cs
@@ -9,47 +9,14 @@ namespace AlarmClock
 {
     class AlarmClock
     {
-        string Id { get; }
-        string myText { get; set; }
-        //TimerCallback tm = new TimerCallback(WakeUp);
-        Timer timer { get; set; }
-        int minutes { get; set; }
-        int hours { get; set; }
+        int Id { get; set; }
+        int Hours { get; set; }
+        int Minutes { get; set; }
 
-        public AlarmClock(string str, int value, int hour, int minute)
+        public void WakeUp() 
         {
-            Id = Guid.NewGuid().ToString();
-            myText = str;
-            minutes = minute;
-            hours = hour;
-            Save();
+            Console.Beep(1000, 1500);
         }
-
-        private int CountTime(int hour, int minute)
-        {
-            return 1;
-        }
-        private static void WakeUp(object obj)
-        {
-            
-        }
-
-        private static void Start()
-        {
-
-        }
-
-        private void Save()
-        {
-            using (FileStream fstream = new FileStream($"{this.Id}.txt", FileMode.OpenOrCreate))
-            {
-                // преобразуем строку в байты
-                string data = $"{this.hours}:{this.minutes}/n{myText}";
-                byte[] array = Encoding.Default.GetBytes(data);
-                // запись массива байтов в файл
-                fstream.Write(array, 0, array.Length);
-                Console.WriteLine("Текст записан в файл");
-            }
-        }
+        void Save() { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Timers;
 
 namespace AlarmClock
 {
     class Program
     {
+        public static void UpdateTime(object sender)
+        {
+            var now = DateTime.Now.ToString("HH:mm:ss");
+            Console.SetCursorPosition(0, 0);
+            Console.Write("\r{0}", now);
+            using (FileStream fstream = new FileStream("Current Time.txt", FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(now);
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+
+        static public System.Timers.Timer Tm = new System.Timers.Timer();
+
+        static void Tm_Tick(object sender, ElapsedEventArgs e)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write("\r{0}", DateTime.Now.ToString());
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            /*
+            TimerCallback tm = new TimerCallback(UpdateTime);
+            Timer timer = new Timer(tm, null, 0, 1000);
+
+            int left = Console.CursorLeft;
+            int top = Console.CursorTop;
+            Thread.Sleep(2000);
+            */
+
+            Tm.Elapsed += new ElapsedEventHandler(Tm_Tick);
+            Tm.Interval = 1000;
+            Tm.Enabled = true;
+            Console.ReadKey(true);
+
+            while (true)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine();
+                Console.WriteLine("Выберите опцию");
+                //for (int i = 0; i < comps.Length; i++)
+                //  Console.WriteLine($"{i} - {comps[i].MethodDescription()}");
+            }
         }
     }
 }
